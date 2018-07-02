f2a70bcaf8b995812133c5ee87c0b4f9f40af7d4
Lindsay Power-Harding <lindsph7@gmail.com>
2018-07-02 12:29:43 -0400

    adding weather widget

--- dev/scripts/Weather.js
+++ dev/scripts/Weather.js
@@ -1,35 +1,92 @@
 import React from 'react';
 import axios from 'axios';
 
+
 class Weather extends React.Component {
     constructor() {
         super();
         this.state = {
-            weather: []
+            temperature: undefined,
+            city: undefined,
+            country: "CA",
+            humidity: undefined,
+            description: undefined,
+            error: undefined
         }
 
-        this.getWeather = this.getWeather.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    componentDidMount() {
-        this.getWeather();
+    handleChange(event) {
+        // console.log('change');
+        // console.log(event.target.value);
+        // console.log(event.target.name);
+
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
 
-    getWeather() {
-        axios.get('http://api.wunderground.com/api/61f0a55cb00602dc/conditions/q/Ontario/Toronto.json') 
+    handleSubmit(event) {
+        event.preventDefault();
+        
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city},CA&APPID=471cdaa149464c85637a4d7ef59bb5fd&units=metric`)
             .then((res) => {
-                console.log(res.data);
-                this.setState({
-                    weather: res.data
-                })
+                console.log(res);
+                if (city) {
+                    this.setState({
+                        temperature: res.data.main.temp,
+                        city: res.data.name,
+                        humidity: res.data.main.humidity,
+                        description: res.data.weather[0].description,
+                    });
+                } else {
+                    this.setState({
+                        temperature: undefined,
+                        city: undefined,
+                        humidity: undefined,
+                        description: undefined,
+                        error: "Please enter the values."
+                    });
+                }
             });
     }
 
     render() {
+        const city = this.state.city;
+        const temperature = this.state.temperature;
+        const humidity = this.state.humidity;
+        const conditions = this.state.description;
+
         return (
-            <div>
-                <div>HEEEEELLLLOOOOOO</div>
+            <div className="weatherWidget">
+                {this.state.temperature ?
+
+                <div className="weatherInfo">
+                    <p>Location: {city}</p>
+                    <p>Temperature: {temperature}</p>
+                    <p>Humidity: {humidity}</p>
+                    <p>Conditions: {conditions}</p>
+                </div>
+
+                :
+                <div className="formRequest">
+                    <form action="" onSubmit={this.handleSubmit}>
+                        <input type="text"
+                        className="city"
+                        id="city"
+                        name="city"
+                        placeholder="City.."
+                        onChange={this.handleChange}
+                        />
+                        <div>Canada</div>
+                        <button>Get Weather</button>
+                    </form>
+                </div>
+                }
             </div>
+            // end of div.weatherWidget
         )
     }
 }
